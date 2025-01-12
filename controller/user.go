5ae49dd8a11d150e69050aea1644fdffcd925bc6
Sigma man <2200345850@qq.com>
2025-01-12 14:31:14 +0800

    创建用户保存至数据库

--- controller/user.go
+++ controller/user.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"bluebell/logic"
 	"bluebell/models"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -30,17 +29,13 @@ func SignUpHandler(c *gin.Context) {
 		})
 		return
 	}
-	//手动对请求参数进行详细的业务规则校验
-	//if len(p.Username) == 0 || len(p.Password) == 0 || len(p.RePassword) == 0 || p.RePassword != p.Password {
-	//	zap.L().Error("SignUp with invalid param")
-	//	c.JSON(http.StatusBadRequest, gin.H{
-	//		"msg": "请求参数有误",
-	//	})
-	//	return
-	//}
-	fmt.Println(p)
 	//2.业务处理
-	logic.SignUp(p)
+	if err := logic.SignUp(p); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": "注册失败",
+		})
+		return
+	}
 	//3.返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"msg": "success",
