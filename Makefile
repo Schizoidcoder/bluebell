33ecdd81b1603b3112d97202710d62ea780a7156
Sigma man <2200345850@qq.com>
2025-01-19 18:10:18 +0800

    修改makefile、添加静态文件、修改路由

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 .PHONY: all build run gotool clean help
 
-BINARY="bluebell"
+BINARY= bluebell
 
 all: gotool build
 
+# CGO_ENABLED 禁用CGO，提高兼容性 GOARCH 目标操作系统架构 -o 指定二进制文件输出目录
 build:
-	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${BINARY}
+	mkdir -p ./bin
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/bluebell
 
 run:
 	@go run ./
--- routes/routes.go
+++ routes/routes.go
@@ -2,12 +2,10 @@ package routes
 
 import (
 	"bluebell/controller"
+	_ "bluebell/docs"
 	"bluebell/logger"
 	"bluebell/middlewares"
 	"net/http"
-	"time"
-
-	_ "bluebell/docs"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,26 +15,30 @@ func SetupRouter(mode string) *gin.Engine {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r := gin.New()
-	r.Use(logger.GinLogger(), logger.GinRecovery(true), middlewares.RateLimitMiddleware(time.Second*2, 1))
+	//r.Use(logger.GinLogger(), logger.GinRecovery(true), middlewares.RateLimitMiddleware(time.Second*2, 1))
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
 
+	r.LoadHTMLGlob("./templates/*")
+	r.Static("./static", "./static")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 	v1 := r.Group("/api/v1")
 	//注册
 	v1.POST("/signup", controller.SignUpHandler)
 	//登陆
 	v1.POST("/login", controller.LoginHandler)
-
+	//根据时间或分数获取帖子列表
+	v1.GET("/posts2", controller.GetPostListHandler2)
+	v1.GET("/community", controller.CommunityHandler)
+	v1.GET("/community/:id", controller.CommunityDetailHandler)
+	v1.GET("/post/:id", controller.GetPostDetailHandler)
+	v1.GET("/posts", controller.GetPostListHandler)
 	v1.Use(middlewares.JWTAuthMiddleware()) //应用JWT认证中间件
 
 	{
-		v1.GET("/community", controller.CommunityHandler)
-		v1.GET("/community/:id", controller.CommunityDetailHandler)
 
 		v1.POST("/post", controller.CreatePostHandler)
-		v1.GET("/post/:id", controller.GetPostDetailHandler)
-		v1.GET("/posts", controller.GetPostListHandler)
-		//根据时间或分数获取帖子列表
-		v1.GET("/posts2", controller.GetPostListHandler2)
-
 		//投票
 		v1.POST("/vote", controller.PostVoteController)
 	}
